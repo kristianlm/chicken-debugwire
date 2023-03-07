bbbbccd36cc2610834c1c7e6e9c136e8d97ac64a
Kristian Lein-Mathisen <kristianlein@gmail.com>
2023-03-07 15:05:50 +0100

    bugfix: no more corrupt blobs when reading >1 page

--- reader.scm
+++ reader.scm
@@ -11,7 +11,7 @@
           (let* ((size (min chunksize len))
                  (next (+ start size)))
             (loop next (- len size)
-                  (kons result (reader start size))))
+                  (kons (reader start size) result)))
           result))))
 
 (define (writer-chunkify writer default-chunksize)
